06142a1e8fc2a8241137168c53b8f4209d895f21
JeonYoungSong <syh45678910@gmail.com>
2020-12-29 11:05:10 +0900

    template, model 약간 수정 Form 을 장고 템플릿으로 구현하는건 잘안돼서 하는중입니다

--- odoyApp/views.py
+++ odoyApp/views.py
@@ -8,11 +8,17 @@ def odoymain(request):
 
 def secondPage(request):
     if(request.method == 'POST'):
-        nickname = request.POST['nickname']
-        ngender = request.POST['gender']
-        nbirth = request.POST['birth']
+        nickname = request.POST['nickname'] #String Type
+        if request.POST['gender'] == 0:
+            ngender = "female"
+        else:
+            ngender = "male"
+        #ngender = request.POST['gender'] # String Type
+
+        nbirth = request.POST['birth'] # DatetimeType
         nlife = request.POST['life']
         new_data = User_Info(name = nickname,gender = ngender,birth = nbirth,life = nlife)
+
         new_data.save()
         print(nickname + " " + ngender + " " + nbirth + " " + nlife)
     return render(request,'secondPage.html')
